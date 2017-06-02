25e85fdfbb0560b032318117cb0df443c46cc16b
Artur Manukian <manukian.artur.94@gmail.com>
2017-06-02 23:24:56 +0300

    Карта без функционала

---
+++ app/src/main/java/com/example/mapsapi/MapsActivity.java
@@ -0,0 +1,34 @@
+package com.example.mapsapi;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Добавление маркера
+        LatLng position = new LatLng(48.4640763, 35.0440359);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 10));
+        mMap.addMarker(new MarkerOptions().position(position).title("Днепр"));
+    }
+}
